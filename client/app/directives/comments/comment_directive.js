67e6d003f1811b9b403980799383414363478eb0
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-05-17 13:20:12 -0600

    cleaned up comment directive

--- client/app/directives/comments/comment_directive.js
+++ client/app/directives/comments/comment_directive.js
@@ -4,19 +4,21 @@ angular.module('Reddit').directive('mbComment', function() {
         replace: true,
         templateUrl: '/directives/comments/comments.html',
         link: function(scope, element, attrs, fn) {
-            scope.comments = scope.post.comments;
+            // scope.comments = scope.post.comments;
             scope.view = {};
+            scope.newComment = {}
 
             scope.submitComment = function(post, comment, commentForm) {
-              var commentCopy = angular.copy(comment);
+              var commentCopy = angular.copy(scope.newComment);
               post.comments.push(commentCopy)
               $('#commentModal_'+post.id).modal('hide');
               $('.modal-backdrop').remove();
               console.log(commentForm);
               commentForm.$setUntouched();
               commentForm.$setPristine();
-              console.log(commentForm);
-              scope.newComment = {};
+              console.log(scope.newComment)
+              scope.newComment= {}
+              console.log(scope.newComment);
             }
             scope.toggleComments = function(post) {
               this.post.showComments = !this.post.showComments;
