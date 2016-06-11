fdb22566bb3ac3ac97f379607ff071d7918d64ac
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-06-11 07:30:31 -0600

    get posts route is working

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,53 +12,41 @@ router.get('/', function(req, res, next) {
   res.send('you did it!');
 });
 router.get('/posts', function(req, res, next) {
-  const results = {};
+  const _posts = []
   knex('posts')
-  .then(function(posts){
-    results.posts = posts;
-  })
-  .then(function () {
-    knex('comments')
-    .then(function(comments) {
-      results.comments = comments;
-      for (var i = 0; i < results.posts.length; i++) {
-        results.posts[i].comments = [];
-        results.posts[i].showComments = false;
-        for (var j = 0; j < results.comments.length; j++) {
-          if (results.posts[i].id === results.comments[j].post_id) {
-            results.posts[i].comments.push(results.comments[j])
-          }
-        }
-      }
-      delete results.comments;
-      res.json(results);
+  .innerJoin('users','posts.user_id','users.id')
+  .select('posts.title','posts.description','posts.votes','posts.image_url','posts.created_at','users.name','posts.id','posts.user_id')
+  .then(function(data){
+    console.log('data: ', data);
+    data.forEach(function(item){
+      _posts.push({
+        id:item.id,
+        user_id:item.user_id,
+        title:item.title,
+        description:item.description,
+        image_url:item.image_url,
+        votes: item.votes,
+        username: item.name,
+        created_at: item.created_at,
+        comments: []
+      })
     })
+    return knex('comments')
+    .innerJoin('users','comments.user_id','users.id')
+    .select('users.name','comments.comment','comments.post_id','comments.id','comments.user_id')
   })
-});
-// router.get('/posts',function (req,res,next) {
-//   const results = {}
-//   knex('posts')
-//   .then(function(posts) {
-//     results.posts = posts
-//   })
-//   .then(function () {
-//   knex('comments')
-//     .then(function (comments) {
-//       results.comments = comments
-//         results.posts.forEach(function(post) {
-//            post.comments = [];
-//            console.log('post: ',post)
-//         })
-//         results.comments.forEach(function(comment) {
-//           if(.id === comment.post_id) {
-//             results.posts.comments.push(comment)
-//           }
-//         })
-//       })
-//       delete results.comments;
-//       res.json(results)
-//
-//   })
-// })
+  .then(function(dataComments){
+    _posts.forEach(function(post){
+      dataComments.forEach(function(comment){
+        console.log('comment: ',comment);
+        if(post.id === comment.post_id){
+          post.comments.push(comment)
+        }
+      })
+    })
+    console.log('posts: ',_posts);
+    res.json(_posts)
+  })
+})
 
 module.exports = router;
