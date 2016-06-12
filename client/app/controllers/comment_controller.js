cd2acf9c830d7e7b093025d94c133a6be1240dcd
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-06-12 08:18:12 -0600

    we are making comments

--- client/app/controllers/comment_controller.js
+++ client/app/controllers/comment_controller.js
@@ -1,25 +1,28 @@
 angular.module('Reddit')
 .controller('CommentCtrl',['$scope','$log','$http','postService',function($scope,$log,$http,postService) {
   $scope.newComment = {}
-  $scope.newComment.user_id = function(){
-    return Number(localStorage.getItem('user_id'))
-  }
-  
+  $scope.newComment.user_id = Number(localStorage.getItem('user_id'))
+
   $scope.submitComment = function(post){
     if(!localStorage.getItem ('token')){
       alert('Please login to make a comment')
     } else {
-      $scope.newComment.post_id = post.id
+      $scope.newComment.post_id = this.post.id
       var commentCopy = angular.copy($scope.newComment)
       postService.newComment(commentCopy).then(function(response){
         $log.info(response)
+      }).then(function(){
+        postService.all().then(function(response) {
+          $scope.view.posts = response.data
+          $scope.newComment = {}
+        })
       })
     }
   }
   $scope.toggleComments = function(post) {
-    $log.info('post: ',post)
-    $log.info('showComments========== ',post.showComments)
-    return post.showComments = !post.showComments
+    $log.info('post: ',this.post)
+    $log.info('showComments========== ',this.post.showComments)
+    return this.post.showComments = !this.post.showComments
   }
 
 
