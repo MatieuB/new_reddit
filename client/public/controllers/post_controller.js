36b61b6ed9502c1c66825c7bea820051ede1eaeb
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-06-19 16:19:07 -0600

    changed a bunch of shit

--- client/public/controllers/post_controller.js
+++ client/public/controllers/post_controller.js
@@ -1,17 +1,14 @@
 angular.module('Reddit')
 .controller('PostCtrl',['$scope','$log','$http','postService',function($scope,$log,$http,postService) {
   // $log.info('currentUser: ', currentUser)
-  $scope.voteChange = function() {
-    $log.info('vote thing clicked');
-    alert('connected!');
-    // postService.changeVotes().then(function(results){
-    //   $log.info('vote results', results)
-    // })
-  };
-  $scope.test = 'testing==========';
+
   $scope.view = {};
   $scope.newPost = {};
   $scope.post = {}
+  $scope.test ="testing......"
+  $scope.voteChange = function(){
+    $log.info('arrow clicked!')
+  }
   postService.all().then(function(response) {
     $scope.view.posts = response.data;
   });
@@ -36,26 +33,5 @@ angular.module('Reddit')
     }
   }
 
-  // $scope.newComment = {}
-  // $scope.newComment.user_id = function(){
-  //   return Number(localStorage.getItem('user_id'))
-  // }
-  //
-  // $scope.submitComment = function(post){
-  //   if(!localStorage.getItem ('token')){
-  //     alert('Please login to make a comment')
-  //   } else {
-  //     $scope.newComment.post_id = post.id
-  //     var commentCopy = angular.copy($scope.newComment)
-  //     postService.newComment(commentCopy).then(function(response){
-  //       $log.info(response)
-  //     })
-  //   }
-  // }
-  // $scope.toggleComments = function(post) {
-  //   $log.info('post: ',post)
-  //   $log.info('showComments========== ',post.showComments)
-  //   return post.showComments = !post.showComments
-  // }
 
 }])
---
+++ client/public/services/post_service.js
@@ -0,0 +1,50 @@
+angular.module('Reddit')
+.factory('postService', ['$http','$log',function ($http,$log) {
+  return {
+    all: function() {
+      return $http.get('http://localhost:4000/api/posts').then(function(response){
+        $log.info(response)
+        return response
+      });
+    },
+    newPost: function(newPost) {
+      return $http.post('http://localhost:4000/api/posts/add',newPost).then(function(response){
+        log.info(response)
+        return response
+      })
+    },
+    getMe: function(){
+      $http.get('http://localhost:4000/api/users/me')
+      .then(function (response) {
+        $log.info('from resolve',response.data)
+        if(response.data.error) {
+          localStorage.clear();
+          // $location.path('/login')
+          return {message: 'need to login'}
+        }
+        return response.data
+      })
+    },
+    deletePost: function(id) {
+      return $http.delete('/api/post/' + id)
+    },
+    editPost: function(pirate) {
+      return $http.put('/api/edit/post/' +pirate.id , post)
+    },
+    getPost: function(id) {
+      return $http.get('/api/post/'+ id)
+    },
+    newComment: function(newComment) {
+      return $http.post('http://localhost:4000/api/comments/add',newComment).then(function(response){
+        $log.info('response form commentpost: ', response)
+        return response
+      })
+    },
+    changeVotes: function(postId){
+      return $http.post('http://localhost:4000/api/votes').then(function(response){
+        $log.info(response);
+        return response;
+      })
+    }
+  }
+}])
