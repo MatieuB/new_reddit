ed258a9ae7cd7210b6fc24dc6bc66d38a6fdc6d4
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-06-21 21:17:06 -0600

    add production env

--- server/knexfile.js
+++ server/knexfile.js
@@ -9,6 +9,14 @@ module.exports = {
       max:10
     }
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool:{
+      min:2,
+      max:10
+    }
+  },
   seeds: {
     directory: '/seeds/'
   }
